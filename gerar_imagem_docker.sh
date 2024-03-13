44ab8621c801d3a600a7e66fb4ab4bd4e31da857
davicarrano <davicarrano@yahoo.com.br>
2024-03-13 15:05:55 -0300

    alterei o caminho da imagem docker no repositorio

--- gerar_imagem_docker.sh
+++ gerar_imagem_docker.sh
@@ -1,5 +1,5 @@
-IMAGEM=registry.ufsj.edu.br:5999/frontend/smartglpi-backend
-VERSAO=v1.0.1
+IMAGEM=registry.ufsj.edu.br:5999/smartglpi/smartglpi-backend
+VERSAO=v1.0.0
 
 docker build -t $IMAGEM:$VERSAO .
 docker push $IMAGEM:$VERSAO
